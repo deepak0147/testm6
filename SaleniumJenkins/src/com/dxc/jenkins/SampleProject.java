83d0725fccd73904b8e5bac272bbce30ed3a8c8b
dchitturi3 <dchitturi3@host.docker.internal>
2020-09-26 20:13:45 +0530

    First Commit

---
+++ SaleniumJenkins/src/com/dxc/jenkins/SampleProject.java
@@ -0,0 +1,11 @@
+package com.dxc.jenkins;
+
+public class SampleProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello Dxc People");
+	}
+
+}
